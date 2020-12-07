17f78e7dc57d7ad8143c9b2ae9eee9802ec2e5f1
tatifp <tatifp@yahoo.com.br>
2020-12-07 10:17:10 -0300

    Estudos do dia 07-12-20

--- introducao/primeiro-exemplo.js
+++ introducao/primeiro-exemplo.js
@@ -1,2 +1,10 @@
 console.log("Javascript Tati funcionando oieee!!!");
 
+
+let nome = "Tatiane"
+console.log (10 + 20)
+
+// uma unica linha
+/*
+bloco comentado
+*/
---
+++ introducao/use-sctrict.js
@@ -0,0 +1,15 @@
+   "use strict"
+      
+   function foo (){
+      let x = 20
+      return x
+    }
+
+    function dobrar(n1, n1){
+        return n1 * n1
+    }
+//foo()
+console.log("Valor é " + foo())
+console.log("Valor é " + dobrar(5, 10))
+
+
